ef7d9568433ca194edca25074e310cbdb63f463d
illenium <illenium@localhost.localdomain>
2019-10-22 07:27:50 -0400

    novos ex

---
+++ poo/todo_v3.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+
+
+class Tarefa:
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.feito = False
+        self.criacao = datetime.now()
+
+    
+
+
+    def concluir(self): 
+        self.feito = True
+
+    def __str__(self):
+        return self.descricao + (' -  (Concluída) ' if self.feito else '')
+
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def __iter__(self): 
+        return self.tarefas.__iter__()
+
+    def add(self, descricao):
+        self.tarefas.append(Tarefa(descricao))
+
+    def pendentes(self):
+        return [tarefa for tarefa in self.tarefas if not tarefa.feito]
+
+    def procurar(self, descricao):
+        # Possível IndexError
+        return [tarefa for tarefa in self.tarefas if tarefa.descricao == descricao][0]
+
+    def __str__(self):
+        return f'{self.nome} ({len(self.pendentes())}) tarefas pendente(s)'
+
+
+def main():
+
+    casa = Projeto('Tarefas de Casa')
+
+    casa.add('Lavar Pratos')
+    casa.add('Lavar pratos')
+
+    print(casa)
+    casa.procurar('Lavar Pratos').concluir()
+    for tarefa in casa.tarefas:
+        print(f' - {tarefa}')
+    print(casa)
+
+
+if __name__ == "__main__":
+    main()
